214a387d06dc957e70a50cbad69752da1be7f789
Michael B <1012481+michaelbraunstein@users.noreply.github.com>
2018-10-29 17:43:38 -0400

    Add files via upload

--- assignment_8.sql
+++ assignment_8.sql
@@ -6,15 +6,16 @@ Assignment 8
 October 23, 2018
 */
 
-DROP TABLE customers CASCADE CONSTRAINTS;
+DROP TABLE customer CASCADE CONSTRAINTS;
 DROP TABLE invoice CASCADE CONSTRAINTS;
-DROP TABLE products CASCADE CONSTRAINTS;
+DROP TABLE product CASCADE CONSTRAINTS;
 DROP TABLE invoice_item CASCADE CONSTRAINTS;
 
-CREATE TABLE customers (
-user_ID NUMBER PRIMARY KEY,
+CREATE TABLE customer (
+customer_id NUMBER PRIMARY KEY,
 fname VARCHAR2(59),
 lname VARCHAR2(59),
+bizname VARCHAR2(59),
 address_1_line_1 VARCHAR2(59),
 address_1_line_2 VARCHAR2(59),
 address_1_city VARCHAR2(11),
@@ -24,50 +25,60 @@ credit_card_1_exp VARCHAR2(5),
 credit_card_1_num VARCHAR2(21),
 account_balance NUMBER(11) DEFAULT (0));
 
-INSERT INTO customers VALUES (1,  'Max', 'Antin',  '950 Flemington St', 'second floor', 'Pittsburgh', 'PA', '15217', '11/29', '6011-999-9999', 0);
---(select user_ID, fname, lname, address_1_line_1, address_1_line_2, address_1_city, address_1_state, address_1_zip, credit_card_1_exp, credit_card_1_num, account_balance  
---  from customers);
-  
---SELECT*FROM   (customers);
-  
---desc customers;
-  
-CREATE TABLE products (
+INSERT INTO customer VALUES (1,  'Max', 'Antin', 'Max Antin Floors',  '950 Flemington St', 'second floor', 'Pittsburgh', 'PA', '15217', '11/29', '6011-999-9999', 0);
+ 
+CREATE TABLE product (
 product_sku NUMBER PRIMARY KEY,
-product_name VARCHAR2(59),
-product_price NUMBER(11) DEFAULT 0);
+product_description VARCHAR2(59) CONSTRAINT product_description unique,
+product_price NUMBER(11,2) DEFAULT 1);
 
-INSERT INTO products (product_sku, product_name, product_price) VALUES (1, "saw", 7);
-INSERT INTO products (product_sku, product_name, product_price) VALUES (2, "miter", 9);
+INSERT INTO product (product_sku, product_description, product_price) VALUES (1, 'saw', 7.00);
+INSERT INTO product (product_sku, product_description, product_price) VALUES (2, 'miter', 9.99);
+INSERT INTO product (product_sku, product_description, product_price) VALUES (3, 'Uncle Johns Dried Pinto Beans', 1);
 --(select product_sku, product_name, product_price  from products);
-SELECT*FROM products;
-
---desc products;  
 
 CREATE TABLE invoice (
 invoice_number NUMBER PRIMARY KEY,
-invoice_item VARCHAR2(9),
+customer_id NUMBER,
+--invoice_item NUMBER,
 order_date DATE, 
 delivery_date DATE);
 
---desc invoice;
+INSERT INTO invoice (invoice_number, customer_id, order_date, delivery_date)  VALUES ( 1, 1, '18-10-18', '18-11-18');
 
 CREATE TABLE invoice_item (
-invoice_number int(9), 
-product_sku int(9), 
-constraint t_pk PRIMARY KEY (invoice_number REFERENCES invoice, product_sku REFERENCES product),
-product_quantity INT(3),
-price (product_quantity REFERENCES invoice * product_price REFERENCES product) NUMBER(11) DEFAULT(0));
+invoice_number number,
+foreign key (invoice_number) REFERENCES invoice,
+product_sku number, 
+primary key (invoice_number, product_sku),
+foreign key (product_sku) REFERENCES product,
+--constraint t_pk PRIMARY Key (invoice_number REFERENCES invoice, product_sku REFERENCES product),
+product_quantity number,
+product_price number(11,2)
+);
 
-
-
-INSERT INTO invoice (invoice_number, invoice_item oder_date, deliver_date)  VALUES ( 1, 1, 10/18/2018, 11/25/2018 );
 --(select invoice_number, invoice_item, order_date, delivery_date from invoice);
 
-INSERT INTO invoice_item (invoice_number, product_sku, product_quantity) VALUES (1, 1, 1);
 
-desc invoice_item;
-desc invoice;
 
-SELECT*FROM invoice;
-SELECT*FROM invoice-item;
+insert into invoice_item (invoice_number, product_sku, product_quantity, product_price)   VALUES(1, 1, 1, 7.00);
+insert into invoice_item (invoice_number, product_sku, product_quantity, product_price)   VALUES(1, 3, 91, 1);
+
+CREATE INDEX product_index ON invoice_item(product_sku);
+
+SELECT * FROM invoice_item;
+SELECT * FROM invoice;
+
+
+SELECT customer.bizname, SUM(invoice_item.product_quantity) AS Beans_Ordered  
+FROM customer 
+INNER JOIN invoice on customer.customer_id = invoice.customer_id
+INNER JOIN invoice_item on invoice.invoice_number = invoice_item.invoice_number
+INNER JOIN product on product.product_sku = invoice_item.product_sku 
+
+WHERE invoice_item.product_sku = 3 
+HAVING SUM (product_quantity) >3
+group by bizname  
+ 
+ 
+;
